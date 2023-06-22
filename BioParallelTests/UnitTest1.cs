574415868fbf9efdea3f77beaee0279145fc524e
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-06-22 21:05:06 +0200

    Added tests directory

---
+++ BioParallelTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BioParallelTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
