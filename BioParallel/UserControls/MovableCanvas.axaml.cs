2d7573c94c53ce855d6c2b8929c347ea1a9123fa
Cyprian Zdebski <cyprianz5mail@gmail.com>
2023-06-27 17:13:47 +0200

    Abstracted away the canvas

---
+++ BioParallel/UserControls/MovableCanvas.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BioParallel.UserControls
+{
+    public partial class MovableCanvas : UserControl
+    {
+        public MovableCanvas()
+        {
+            InitializeComponent();
+        }
+    }
+}
